55dd49a316bdc0f4b71a7e4574262f53184793d5
matmartinelli <matteomartinelli85@gmail.com>
2026-03-08 19:35:28 +0100

    added first reconstruction test

---
+++ Cosmology/cosmo.py
@@ -0,0 +1,61 @@
+import sys
+import numpy  as np
+import pandas as pd
+
+from copy import deepcopy
+
+from scipy.interpolate import UnivariateSpline
+
+import camb
+
+#THIS IS ALL TEMPORARY!!!!
+
+class CalcCosmology:
+
+    def __init__(self,params):
+
+        self.zcalc  = np.linspace(0.,5,1000)
+
+        self.params = self.cambify(params)
+
+        self.results = self.get_cosmology()
+
+    def get_cosmology(self):
+
+        zcamb = np.logspace(-3,np.log10(max(self.zcalc)),250)
+
+
+        pars = camb.set_params(redshifts=zcamb,silent=True,**self.params)
+        results = camb.get_results(pars)
+
+        camb_fsig8 = np.array(results.get_fsigma8())
+        camb_sig8  = np.array(results.get_sigma8())
+        camb_f     = camb_fsig8/camb_sig8
+
+        hubble = UnivariateSpline(self.zcalc,results.hubble_parameter(self.zcalc),s=0,k=4)
+        growth = UnivariateSpline(zcamb,np.flip(camb_f),s=0,k=4)
+
+        Om = (self.params['ombh2']+self.params['omch2']+self.params['omnuh2'])/(self.params['H0']/100)**2
+
+        output = {'Hubble': {'f': hubble,
+                             'd1': lambda x: 3*self.params['H0']**2*Om*(1+x)**2/hubble(x),
+                             'd2': hubble.derivative(n=2)},
+                  'growth_rate': {'f': growth}}
+
+        return output
+
+    def cambify(self,params):
+
+        new_params = deepcopy(params)
+
+        if 'omegam' in params:
+            new_params['ombh2']  = 0.05*(new_params['H0']/100)**2
+            new_params['omnuh2'] = 0.0006
+            new_params['omch2']  = new_params.pop('omegam')*(new_params['H0']/100)**2-new_params['ombh2']-new_params['omnuh2']
+
+        new_params['WantTransfer'] = True
+
+        return new_params
+
+
+
--- tools/samplers_interface.py
+++ tools/samplers_interface.py
@@ -19,7 +19,7 @@ class SamplersInterface:
         else:
             sys.exit('Unknown sampler: {}'.format(sampler))
 
-    def run_nautilus(self,parameters,likelihood):
+    def run_nautilus(self,parameters,likelihood,derived=None):
     
         if self.run_options == 'poor':
             sets = {'num_threads': 1,
@@ -47,14 +47,31 @@ class SamplersInterface:
             print('Loaded prior into Nautilus with dimension',prior.dimensionality())
             print('Prior keys: ',prior.keys)
             print('Starting to sample with Nautilus...')
-        nautilus_options = {k:v for k,v in sets.items() if k != 'num_threads'}
-        sampler = Sampler(prior,likelihood,**nautilus_options,n_dim=ndim)
-        sampler.run(verbose=True)
 
-        points, log_w, log_l = sampler.posterior(equal_weight=True)
-        nautilus_dict = {par: parameters[par]['latex'] for par in prior.keys}
+        if derived != None:
+            derived_pars = list(derived.keys())
+            blob_vec     = [(par, float) for par in derived_pars]
+
+
+        nautilus_options = {k:v for k,v in sets.items() if k != 'num_threads'}
+
+        if derived == None:
+            sampler = Sampler(prior,likelihood,**nautilus_options,n_dim=ndim)
+        else:
+            sampler = Sampler(prior,likelihood,**nautilus_options,blobs_dtype=blob_vec)
+
+        sampler.run(verbose=self.chatty)
+
+        if derived == None:
+            points, log_w, log_l = sampler.posterior(equal_weight=True)
+            nautilus_dict = {par: parameters[par]['latex'] for par in prior.keys}
+            results = pd.DataFrame(np.c_[points, np.exp(log_w), -log_l],columns=list(nautilus_dict.keys())+['weight','minuslogpost'])
+        else:
+            points, log_w, log_l, derived_points = sampler.posterior(equal_weight=True,return_blobs=True)
+            derived_array = np.array([np.array(list(der)) for der in derived_points])
+            nautilus_dict = {par: parameters[par]['latex'] for par in prior.keys} | {par: derived[par]['latex'] for par in derived_pars}
+            results = pd.DataFrame(np.c_[points, derived_array, np.exp(log_w), -log_l],columns=list(nautilus_dict.keys())+['weight','minuslogpost'])
     
-        results = pd.DataFrame(np.c_[points, np.exp(log_w), -log_l],columns=list(nautilus_dict.keys())+['weight','minuslogpost'])
         results = results[['weight','minuslogpost']+list(nautilus_dict.keys())]
 
         if self.chatty:
