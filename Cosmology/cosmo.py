f9144582d8336993bdbf752d53f5507fe28b45d3
matmartinelli <matteomartinelli85@gmail.com>
2026-03-25 12:33:40 +0100

    added DM to output of cosmo module

--- Cosmology/cosmo.py
+++ Cosmology/cosmo.py
@@ -40,6 +40,7 @@ class CalcCosmology:
         output = {'Hubble': {'f': hubble,
                              'd1': lambda x: 3*self.params['H0']**2*Om*(1+x)**2/hubble(x),
                              'd2': hubble.derivative(n=2)},
+                  'DM': {'f': lambda x: (1+x)*results.angular_diameter_distance(x)}, #WARNING! CURVATURE!
                   'dL': {'f': lambda x: (1+x)**2*results.angular_diameter_distance(x)},
                   'growth_rate': {'f': growth}}
 
