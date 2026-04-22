7240af6c01eeeea4bb93e1d7af40fd1ded71f877
matmartinelli <matteomartinelli85@gmail.com>
2026-04-22 17:46:47 +0200

    added multiple reconstructions

--- derived_function/reconstructor.py
+++ derived_function/reconstructor.py
@@ -46,12 +46,11 @@ class DerivedFunction:
             if arg.startswith(recon_key + '_'):
                 component = arg[len(recon_key)+1:]
                 return recon_key, component
-        # Fallback to last underscore if no match found in keys
         parts = arg.split('_')
         return "_".join(parts[:-1]), parts[-1]
 
     def _get_required_params(self, func):
-        """Introspects lambda and identifies GP components vs coordinate 'x'."""
+        """Introspects logic and identifies GP components vs coordinate 'x'."""
         all_args = inspect.getfullargspec(func).args
         gp_info = []
         needs_x = False
@@ -60,18 +59,15 @@ class DerivedFunction:
             if arg == 'x':
                 needs_x = True
                 continue
-
             recon_key, component = self._split_arg(arg)
             gp_info.append({'recon_key': recon_key, 'comp': component, 'full_arg': arg})
 
         return gp_info, needs_x, all_args
 
     def get_realizations(self, Nreals):
-        """Generates realizations while correctly parsing multi-output column names."""
+        """Generates realizations for all GP components."""
         realizations = {}
-
         for key, cov in self.cov_dict.items():
-            # Correctly handle columns like 'd1_1_0' or 'f_0'
             def parse_col(col):
                 parts = col.split('_')
                 idx = int(parts[-1])
@@ -85,47 +81,70 @@ class DerivedFunction:
                 multivariate_normal(mean=mean, cov=cov.values, allow_singular=True).rvs(size=Nreals),
                 columns=cov.columns
             )
-
         return realizations
 
-    def run_realizations(self, derived_logic, derived_name):
-        gp_info, needs_x, arg_order = self._get_required_params(derived_logic)
+    def run_realizations(self, logic_list, name_list):
+        """
+        Modified to handle a list of logic statements and compute joint correlations.
+        """
+        all_reals = []
+        
+        # Pre-calculate param info for each logic
+        logics_info = [self._get_required_params(l) for l in logic_list]
 
-        func_reals = []
         for ind in range(self.Nreals):
-            derived_values = []
-            for i in range(self.N_recon):
-                call_args = []
-                for arg_name in arg_order:
-                    if arg_name == 'x':
-                        call_args.append(self.x_recon[i])
-                    else:
-                        recon, comp = self._split_arg(arg_name)
-                        call_args.append(self.realizations[recon].iloc[ind][f"{comp}_{i}"])
-                derived_values.append(derived_logic(*call_args))
-            func_reals.append(derived_values)
+            joint_vector = []
+            for logic, (gp_info, needs_x, arg_order) in zip(logic_list, logics_info):
+                derived_vals = []
+                for i in range(self.N_recon):
+                    call_args = []
+                    for arg_name in arg_order:
+                        if arg_name == 'x':
+                            call_args.append(self.x_recon[i])
+                        else:
+                            recon, comp = self._split_arg(arg_name)
+                            call_args.append(self.realizations[recon].iloc[ind][f"{comp}_{i}"])
+                    derived_vals.append(logic(*call_args))
+                joint_vector.extend(derived_vals)
+            all_reals.append(joint_vector)
 
-        data_array = np.array(func_reals)
+        data_array = np.array(all_reals)
         mean_func  = np.mean(data_array, axis=0)
         cov_func   = np.cov(data_array, rowvar=False)
 
+        # Generate joint labels: [name1_0...name1_N, name2_0...name2_N]
+        all_labels = []
+        for name in name_list:
+            all_labels += [f"{name}_{i}" for i in range(self.N_recon)]
+
         sample = GaussianND(mean_func, cov_func, is_inv_cov=False,
-                            names=[f"{derived_name}_{i}" for i in range(self.N_recon)]).MCSamples(self.Nsamples)
+                            names=all_labels).MCSamples(self.Nsamples)
         return sample
 
-    def run_sampling(self, derived_logic, derived_name, sigma_width=5):
-        gp_info, needs_x, arg_order = self._get_required_params(derived_logic)
+    def run_sampling(self, logic_list, name_list, sigma_width=5):
+        """
+        Modified to handle a list of logic statements and compute joint correlations.
+        """
+        # 1. Aggregate all required parameters from all logics
+        all_gp_info = []
+        logics_info = []
+        for logic in logic_list:
+            info, nx, args = self._get_required_params(logic)
+            all_gp_info.extend(info)
+            logics_info.append((info, nx, args))
+
+        # Unique parameters for priors
+        unique_params = {item['full_arg']: item for item in all_gp_info}.values()
 
         if self.chatty:
-            print(f"Sampling GP components: {[g['full_arg'] for g in gp_info]}")
+            print(f"Sampling joint GP components: {[p['full_arg'] for p in unique_params]}")
 
-        # 1. Prepare Priors
+        # 2. Prepare Priors
         parameters = {}
-        for item in gp_info:
+        for item in unique_params:
             recon_key, component, arg_name = item['recon_key'], item['comp'], item['full_arg']
             df_recon = self.recon_dict[recon_key]
             df_cov = self.cov_dict[recon_key]
-
             labels = [f"{component}_{i}" for i in range(self.N_recon)]
             sigmas = np.sqrt(np.diag(df_cov.loc[labels, labels].values))
 
@@ -137,16 +156,16 @@ class DerivedFunction:
                     'latex': rf'${arg_name}_{{{i}}}$'
                 }
 
-        # 2. Likelihood Preparation
+        # 3. Likelihood Preparation (includes all components across all logics)
         likeparts = {}
         for recon_key, df_recon in self.recon_dict.items():
             df_cov = self.cov_dict[recon_key]
-            requested_comps = [g['comp'] for g in gp_info if g['recon_key'] == recon_key]
+            # Get components from any logic that belong to this recon
+            requested_comps = list(set(g['comp'] for g in all_gp_info if g['recon_key'] == recon_key))
             
             if not requested_comps:
                 continue
 
-            # Ensure we match components like 'd1_1' in 'd1_1_0'
             keep_labels = [col for col in df_cov.columns if "_".join(col.split('_')[:-1]) in requested_comps]
             reduced_cov = df_cov.loc[keep_labels, keep_labels]
 
@@ -155,7 +174,6 @@ class DerivedFunction:
                 'data_vec': np.array([df_recon.iloc[int(l.split('_')[-1])]["_".join(l.split('_')[:-1])] 
                                       for l in keep_labels])
             }
-
             try:
                 likeparts[recon_key]['inv_cov'] = np.linalg.inv(reduced_cov.values)
             except np.linalg.LinAlgError:
@@ -170,19 +188,22 @@ class DerivedFunction:
 
             loglike = -0.5 * chi2
             
-            derived_values = []
-            for i in range(self.N_recon):
-                call_args = []
-                for arg_name in arg_order:
-                    if arg_name == 'x':
-                        call_args.append(self.x_recon[i])
-                    else:
-                        call_args.append(param_dict[f"{arg_name}_{i}"])
-                derived_values.append(derived_logic(*call_args))
+            all_derived_values = []
+            for logic, (_, _, arg_order) in zip(logic_list, logics_info):
+                for i in range(self.N_recon):
+                    call_args = []
+                    for arg_name in arg_order:
+                        if arg_name == 'x':
+                            call_args.append(self.x_recon[i])
+                        else:
+                            call_args.append(param_dict[f"{arg_name}_{i}"])
+                    all_derived_values.append(logic(*call_args))
                 
-            return tuple([loglike] + derived_values)
+            return tuple([loglike] + all_derived_values)
 
-        derived_names = {f"{derived_name}_{i}": {'latex': rf'${derived_name}_{{{i}}}$'} 
-                         for i in range(self.N_recon)}
+        derived_names = {}
+        for name in name_list:
+            for i in range(self.N_recon):
+                derived_names[f"{name}_{i}"] = {'latex': rf'${name}_{{{i}}}$'}
 
         return self.sampler.run(parameters, likelihood, derived=derived_names)
